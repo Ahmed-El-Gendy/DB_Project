a5176b8e5ab1a2bfbcd54bf5814f48127583f0bc
Ahmed El Gendy <elgendyahmed753@gmail.com>
2024-05-07 21:33:59 +0300

    update bill

--- bill.py
+++ bill.py
@@ -16,12 +16,12 @@ def insert_bill(guest_id, receptionist_id):
         sql = "SELECT name FROM guest WHERE id = %s"
         cursor.execute(sql, (guest_id,))
         results = cursor.fetchall()
-        check.append(str(f"name: {results[0][0]}"))
+        check.append(str(f"{results[0][0]}"))
         sql = "SELECT price_per_night, interval_duration FROM room WHERE guest_id = %s"
         cursor.execute(sql, (guest_id,))
         results = cursor.fetchall()
         total += int(results[0][0] * results[0][1])
-        check.append(str(f"price per night: {results[0][0]}\nnum of days: {results[0][1]}\n"))
+        check.append(str(f"Price per night: {results[0][0]}\nNum of days: {results[0][1]}\n"))
 
         sql = "SELECT menu.price, guest_orders.number_of_order, menu.name FROM guest_orders join menu on guest_orders.guest_id = %s and guest_orders.meal_id = menu.id"
         cursor.execute(sql, (guest_id,))
@@ -29,7 +29,7 @@ def insert_bill(guest_id, receptionist_id):
         for rwo in results:
             total += int(rwo[0]) * int(rwo[1])
             check.append(str(f"name: {rwo[2]} price: {rwo[0]} count: {rwo[1]}"))
-        check.append(str(f"total: {total}"))
+        check.append(str(f"{total}"))
         sql = "INSERT INTO bill (total_price, guest_id, date_of_check_out, receptionist_id) VALUES (%s, %s, NOW(), %s)"
         data = (total, guest_id, receptionist_id)
         cursor.execute(sql, data)
